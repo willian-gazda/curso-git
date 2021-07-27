e18803eeca5628a103a272dcd9823641bed6acc7
Willian Gazda <gitwilliangazda@gmail.com>
2021-07-27 15:37:22 -0300

    alteracao bom dia 2x projetoxxxx

--- projeto-teste2/Program.cs
+++ projeto-teste2/Program.cs
@@ -4,6 +4,8 @@ namespace projeto_teste2 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("boa  tarde");
+            Console.WriteLine("boa  tarde xxxxxxxx");
         }
     }
 }
